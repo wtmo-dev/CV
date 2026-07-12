4ce63af9f7ad457825571c6bb6a231bf4b16d46a
W-T-M-O <wtmo_dev@naver.com>
2026-07-12 21:08:35 +0900

    feat/improve experience section and update data

--- src/entities/experience/ui/ExperienceCard.tsx
+++ src/entities/experience/ui/ExperienceCard.tsx
@@ -1,4 +1,4 @@
-import {Experience} from "../model/experience";
+import type { Experience } from "../model/interface";
 
 interface Props{
     experience:Experience;
@@ -8,19 +8,61 @@ export default function ExperienceCard({
     experience
 }:Props){
     return (
-        <div>
-            <h1 className="text-4xl font-bold">
-            {experience.company}
-            </h1>
-            <h2 className="text-xl">
-            {experience.position}
-            </h2>
-            <h2 className="text-xl">
-            {experience.period}
-            </h2>
-            <p>
-            {experience.description}
+        <article
+            className="
+                w-full
+
+                rounded-2xl
+                border
+                border-zinc-200
+
+                p-6
+                mb-4
+
+                transition
+                hover:bg-zinc-50
+            "
+        >
+            {/* Header */}
+            <div
+                className="
+                    flex
+                    flex-col
+                    gap-2
+
+                    md:flex-row
+                    md:items-start
+                    md:justify-between
+                "
+            >
+                <div>
+                    <h2 className="text-3xl font-bold">
+                        {experience.company}
+                    </h2>
+
+                    <p className="mt-1 text-sm text-zinc-500">
+                        {experience.period}
+                    </p>
+
+                    <p className="mt-1 text-sm text-zinc-500">
+                        {experience.position}
+                    </p>
+                </div>
+            </div>
+
+            {/* Description */}
+            <p
+                className="
+                    mt-5
+
+                    text-zinc-700
+                    leading-7
+
+                    line-clamp-2
+                "
+            >
+                {experience.description}
             </p>
-        </div>
+        </article>
     )
 }
